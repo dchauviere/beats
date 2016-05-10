62904a588b5c034bcd429e2d16bb258a29a78698
David Chauviere <david.chauviere@orange.com>
2016-05-10 17:16:40 +0200

    Add syslog output

--- libbeat/publisher/publish.go
+++ libbeat/publisher/publish.go
@@ -21,6 +21,7 @@ import (
 	_ "github.com/elastic/beats/libbeat/outputs/kafka"
 	_ "github.com/elastic/beats/libbeat/outputs/logstash"
 	_ "github.com/elastic/beats/libbeat/outputs/redis"
+	_ "github.com/elastic/beats/libbeat/outputs/syslog"
 
 	// load support output codec
 	_ "github.com/elastic/beats/libbeat/outputs/codecs/format"
